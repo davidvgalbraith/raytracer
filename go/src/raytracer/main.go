fa0748fab556bd09a96ebb9adb3510fde9030015
Dave <davidvgalbraith@gmail.com>
2016-04-16 23:51:46 -0700

    use go routines

--- go/src/raytracer/main.go
+++ go/src/raytracer/main.go
@@ -11,12 +11,27 @@ func main() {
 	scene := scene.GetScene()
 	img := image.NewRGBA(image.Rect(0, 0, scene.Camera.PixelsX, scene.Camera.PixelsY))
 	bounds := img.Bounds()
+	routinesDone := make(chan int)
+	numRoutines := 100
+	rowsPerRoutine := (bounds.Max.Y - bounds.Min.Y) / numRoutines
 
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			color := coloring.CalculateColor(scene, x, y)
-			img.Set(x, y, color)
-		}
+	for r := 0; r < numRoutines; r++ {
+		routine := r
+		go func() {
+			start := bounds.Min.Y + rowsPerRoutine*routine
+			for row := start; row < start+rowsPerRoutine; row++ {
+				for x := bounds.Min.X; x < bounds.Max.X; x++ {
+					color := coloring.CalculateColor(scene, x, row)
+					img.Set(x, row, color)
+				}
+			}
+
+			routinesDone <- routine
+		}()
+	}
+
+	for doneRoutine := 0; doneRoutine < numRoutines; doneRoutine++ {
+		<-routinesDone
 	}
 
 	common.WriteImage(img)
